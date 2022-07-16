e4e919ab71ad0a22a78e50674346e99e2913bd30
QuentonPow311 <quenpowell@gmail.com>
2022-07-16 13:24:14 -0400

    sending index and show data

--- breads/README.md
+++ breads/README.md
@@ -1,1 +1,6 @@
 # Breads
+// SHOW
+breads.get('/:arrayIndex', (req, res) => {
+  res.send(Bread[req.params.arrayIndex])
+})
+
--- breads/server.js
+++ breads/server.js
@@ -3,4 +3,22 @@ const express = require('express')
 //Configuration
 require('dotenv').config()
 const PORT = process.env.PORT
-console.log(PORT)
+const app = express()
+
+//ROUTES
+app.get('/', (req, res) => {
+res.send('Welcome to an Awesome app about breads!')
+
+})
+
+// //Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
+//LISTEN
+app.listen(PORT, () => {
+
+    console.log('listening on port', PORT);
+})  
+
+
